82ca3cdfa33d47c37e86f3b50efff07012f73aa7
Glauber <grobis_mn@hotmail.com>
2017-05-14 19:03:35 -0300

    Pedido

---
+++ BitFour.LojaVirtual.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,40 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace BitFour.LojaVirtual.Dominio.Entidades
+{
+    public class Pedido
+    {
+
+        //data annotations para validaçao
+        [Required(ErrorMessage = "Informe seu nome")]
+        public string NomeCliente { get; set; }
+
+        [Display(Name = "Cep:")]
+        public string Cep { get; set; }
+
+        [Display(Name = "Endereço:")]
+        [Required(ErrorMessage = "Informe seu endereço")]
+        public string Endereco { get; set; }
+
+        [Display(Name = "Complemento:")]
+        public string Complememento { get; set; }
+
+        [Required(ErrorMessage = "Informe sua cidade:")]
+        [Display(Name = "Cidade")]
+        public string Cidade { get; set; }
+
+        [Display(Name = "Bairro")]
+        public string Bairro { get; set; }
+
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Informe seu email:")]
+        [EmailAddress(ErrorMessage = "E-mail Inválido")]
+        public string Email { get; set; }
+
+        [Display(Name = "Embrulha para presente!")]
+        public bool EnbrulhaPresente { get; set; }
+
+    }
+}

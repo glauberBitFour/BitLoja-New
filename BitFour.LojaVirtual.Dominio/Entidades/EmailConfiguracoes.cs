8ad8ec0337b1c04f88e704bd1cb49409ad6f8f94
Glauber <grobis_mn@hotmail.com>
2017-05-20 15:45:26 -0300

    Classe de processar email

---
+++ BitFour.LojaVirtual.Dominio/Entidades/EmailConfiguracoes.cs
@@ -0,0 +1,14 @@
+﻿namespace BitFour.LojaVirtual.Dominio.Entidades
+{
+    public class EmailConfiguracoes
+    {
+        public bool UsarSsl = false;
+        public string ServidorSmtp = "smtp.bitfour.com.br";
+        public int ServidorPorta = 587;
+        public string Usuario = "Glauber";
+        public bool EscreverArquivo = false;
+        public string PastaDoArquivo = @"c:\envioenail";
+        public string Para = "grobis@hotmail.com";
+        public string De = "glaubercasttro@gmail.com";
+    }
+}
---
+++ BitFour.LojaVirtual.Dominio/Entidades/EmailProcessarPedido.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitFour.LojaVirtual.Dominio.Entidades
+{
+   public class EmailProcessarPedido
+   {
+       private readonly EmailConfiguracoes _emailConfiguracoes;
+
+       public EmailProcessarPedido(EmailConfiguracoes configuracoes)
+       {
+           this._emailConfiguracoes = configuracoes;
+       }
+
+       public void ProcessarPedido( Carrinho carrinho,Pedido pedido)
+       {
+           using (var smtpClient = new SmtpClient())
+           {
+               smtpClient.EnableSsl = _emailConfiguracoes.UsarSsl;
+               smtpClient.Host = _emailConfiguracoes.ServidorSmtp;
+               smtpClient.Port = _emailConfiguracoes.ServidorPorta;
+               smtpClient.UseDefaultCredentials = false;
+               smtpClient.Credentials = new NetworkCredential(_emailConfiguracoes.Usuario,
+                   _emailConfiguracoes.ServidorSmtp);
+
+               if (_emailConfiguracoes.EscreverArquivo)
+               {
+                   smtpClient.DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory;
+                   smtpClient.PickupDirectoryLocation = _emailConfiguracoes.PastaDoArquivo;
+                   smtpClient.EnableSsl = false;
+                   StringBuilder body = new StringBuilder()
+                       .AppendLine("Novo Pedido")
+                       .AppendLine("-----------")
+                       .AppendLine("Itens")
+                       ;
+                   foreach (var item in carrinho.ItensCarrinho)
+                   {
+                       var total = item.Produto.Preco*item.Quantidade;
+                       body.AppendFormat("{0} x {1} (subtotal:{2:c}",
+                           item.Quantidade, item.Produto.Nome, total);
+
+                   }
+                   body.AppendFormat("Valor Total do Pedido:{0:c}", carrinho.ObterValorTotal())
+                       .AppendLine("------------------------------")
+                       .AppendLine("Enviar para:")
+                       .AppendLine(pedido.NomeCliente)
+                       .AppendLine(pedido.Endereco ?? "")
+                       .AppendLine(pedido.Cidade ?? "")
+                       .AppendLine(pedido.Complememento ?? "")
+                       .AppendLine("-----------------------------")
+                       .AppendFormat("Para presente?: {0}", pedido.EnbrulhaPresente ? "Sim" : "Nao");
+
+                    MailMessage mailMessage = new MailMessage(
+                        _emailConfiguracoes.De,
+                        _emailConfiguracoes.Para,
+                        "Novo Pedido",body.ToString());
+
+
+                   if (_emailConfiguracoes.EscreverArquivo)
+                   {
+                       mailMessage.BodyEncoding = Encoding.GetEncoding("ISO-8859-1");
+                        smtpClient.Send(mailMessage);
+                   }
+
+
+
+               }
+           }
+       }
+   }
+}

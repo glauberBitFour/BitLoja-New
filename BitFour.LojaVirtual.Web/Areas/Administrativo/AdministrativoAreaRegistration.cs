8dc20740c10d9c124ee94151ee3d087fa801b0d9
Glauber <grobis_mn@hotmail.com>
2017-05-26 19:38:04 -0300

    area admin

---
+++ BitFour.LojaVirtual.Web/Areas/Administrativo/AdministrativoAreaRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace BitFour.LojaVirtual.Web.Areas.Administrativo
+{
+    public class AdministrativoAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Administrativo";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Administrativo_default",
+                "Administrativo/{controller}/{action}/{id}", defaults: new
+                {
+                    controller="Produto", action = "Index", id = UrlParameter.Optional
+                }, namespaces: new [] { "BitFour.LojaVirtual.Web.Areas.Administrativo.Controllers" }
+            );
+        }
+    }
+}
